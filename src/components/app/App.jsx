2f44764ce3a2c52f8456716f5fa4bdfbcc347687
Erich Sauer <erich.sauer@gmail.com>
2021-05-20 11:04:53 -0700

    partial lab - will complete and re-submit :)

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CharacterList from '../characters/CharacterList';
+import Controls from './Controls';
+import Header from './Header';
 
-export default function App() {
-  return <h1>Hello World</h1>;
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Controls />
+    <Switch>
+      <Route exact path="/" component={CharacterList} />
+    </Switch>
+  </Router>
+);
+
+export default App;
---
+++ src/components/characters/CharacterList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Character from './Character';
+
+function CharacterList(props) {
+  return <Character />;
+}
+
+CharacterList.propTypes = {};
+
+export default CharacterList;
